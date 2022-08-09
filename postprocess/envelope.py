b914d9648bae741f703c447f0453914d3341acb4
Rüdiger Göbl <r.goebl@tum.de>
2022-08-09 09:21:18 +0200

    switched to hilbert fir filter

--- postprocess/envelope.py
+++ postprocess/envelope.py
@@ -3,4 +3,11 @@ import scipy.signal
 
 
 def envelope(data, axis=0):
-    return np.abs(scipy.signal.hilbert(data, axis=axis))
+    # return np.abs(scipy.signal.hilbert(data, axis=axis))
+    rejection_band = 0.1  # relative frequency
+    hilbert_fir = scipy.signal.remez(11, [rejection_band, 0.5 - rejection_band], [1], type='hilbert')
+    if axis == 0 and data.ndim == 2:
+        hilbert_fir = np.expand_dims(hilbert_fir, axis=-1)
+    else:
+        raise NotImplementedError
+    return np.abs(data - 1j * scipy.signal.convolve(data, hilbert_fir, 'same'))
